224c3a5480c093dc9b5f31384941c3c541e8a1ee
vladimir0b88 <vladimir0b88@gmail.com>
2024-04-29 22:31:38 +0300

    Установил Blazor.Bootstrap и настроил начальный шаблон

--- BlazorWebAssembly/Program.cs
+++ BlazorWebAssembly/Program.cs
@@ -8,6 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazorBootstrap();
+
 builder.Services.AddScoped(sp => new HttpClient 
 {
     BaseAddress = new Uri("http://localhost:5297")
